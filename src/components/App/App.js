68ccb23f0fb82bde76d7bc9e54fa6fbaf9714af0
Joana Brito <joanafiorentini@hotmail.com>
2021-11-03 10:38:15 -0700

    Add Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useStatem, useEffect } from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+// import Header from './Header/Header';
+// import Footer from './Footer/Footer';
+// import AllNews from './AllNews/AllNews';
+import logo from '../../logo.svg';
 import './App.css';
 
 function App() {
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/' render={() => (
+          <header className="App-header">
+          {/* <Header/>
+          <AllNews/>
+          <Footer/> */}
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              <code>I'm working great!!!</code> 
+            </p>
+            </header>
+          )}/> 
+        <Route exact path='/all-news/:id' render={() => (
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          )} />
+       
+      </Switch>
     </div>
   );
 }
