49605e34c14a054665255fa6bf37abae462f7fab
Joana Brito <joanafiorentini@hotmail.com>
2021-11-03 12:27:08 -0700

    Add Style to search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,8 +5,8 @@ const Search = () => {
     return (
         <form className='search-fiel'>
             <input
-                type='search'
                 className='search-bar'
+                type='search'
                 placeholder='SEARCH'
                 name='userInput'
                 // value={userInput}
@@ -16,10 +16,10 @@ const Search = () => {
                 className='search-btn'
                 type='submit'
                 // onClick={() => handleClick()}
-                >
+            >
                 <img className='mag-glass' src='https://cdn2.vectorstock.com/i/1000x1000/33/01/magnifying-glass-orange-round-search-icon-vector-20353301.jpg' alt='Search Magnifying Glass'/>
 
-                </button>
+            </button>
         </form>
     )
 }
