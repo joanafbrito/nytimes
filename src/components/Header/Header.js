dd148c74ce26fc680f479d7244569bad3a04ae7a
Joana Brito <joanafiorentini@hotmail.com>
2021-11-03 11:01:41 -0700

    Start Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import Search from './Search/Search';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <Link to='/'>
+                <h1 className='company-name'>NY Times</h1>
+            </Link>
+            <p className='search'> search </p>
+            {/* <Search/> */}
+
+        </header>
+    )
+}
+
+export default Header;
