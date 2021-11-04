2a80f088bd60168d89e1e023371fe7f4a22b7c87
Joana Brito <joanafiorentini@hotmail.com>
2021-11-03 23:12:47 -0700

    Add api call and loading

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { getNewsData } from '../../utils/apiCalls';
 // import AllNews from '../AllNews/AllNews';
 import logo from '../../logo.svg';
 import './App.css';
@@ -10,26 +11,30 @@ import './App.css';
 const App = () => {
 
 const [news, setNews] = useState('')
+const [category, setCategory] = useState('everything')
 const [error, setError] = useState('')
-const [isLoading, setLoading] = useState(false)
+const [isLoading, setIsLoading] = useState(false)
 
 
 
-// const invokeNewsData = async() => {
-//   setLoading(true)
-//   try {
-//     const res = await getNewsData()
-//     checkErrors(res)
-//     const returnedNews = await res.json()
-//     console.log(returnedNews)
-//     setNews(returnedNews)      
-//   } catch (err) {
-//     console.log(`Error: ${err}`)
-//   }
-// }
+const invokeNewsData = async(input) => {
+  setIsLoading(true)
+  try {
+    const res = await getNewsData(input || category)
+    checkErrors(res)
+    const returnedNews = await res.json()
+    console.log(returnedNews.response.docs)
+    setNews(returnedNews.response.docs) 
+    // setCategory(category)
+    setIsLoading(false)     
+  } catch (err) {
+    console.log(`Error: ${err}`)
+  }
+}
 
 
 const checkErrors = (res) => {
+  console.log(res.status)
   if (!res.ok) {
     setError(res.status)
   }
@@ -39,9 +44,15 @@ const checkErrors = (res) => {
 //   setError('')
 // }
 
-// useEffect(() => {
-//   invokeNewsData()  
-// }, [news])
+const updateCategory = (input) => {
+  console.log(input)
+  setCategory(input)
+  // invokeNewsData(input)
+}
+
+useEffect(() => {
+  invokeNewsData()  
+}, [category])
 
 
   return (
@@ -49,14 +60,18 @@ const checkErrors = (res) => {
       <Switch>
         <Route exact path='/' render={() => (
           <section className='full-page'>
-            <Header/>
-            {/* <AllNews/> */}
-            <main className='main-body'>
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                <code>I'm working great!!!</code> 
-              </p>
-            </main>
+            <Header updateCategory={ updateCategory }/>
+            {isLoading ? (
+                <h1> Loading ... </h1>
+              ) : (
+              <main className='main-body'>
+                {/* <AllNews/> */}
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  <code>I'm working great!!!</code> 
+                </p>
+              </main> )
+           }
             <Footer/>
             </section>
           )}/> 
