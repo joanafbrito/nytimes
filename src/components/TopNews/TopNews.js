967e3db43086acffffd549bfc10b617caef13851
Joana Brito <joanafiorentini@hotmail.com>
2021-11-05 00:47:49 -0700

    Add styling

--- src/components/TopNews/TopNews.js
+++ src/components/TopNews/TopNews.js
@@ -1,35 +1,26 @@
-import React,{ useState } from 'react';
+import React from 'react';
 // import { Link } from 'react-router-dom';
 import './TopNews.css';
 
-const TopNews = ({ topNewsData }) => {
-
-    // const [randomNews, setRandomNews] = useState([]);
-    console.log( '-------', topNewsData)
-    const randomTopNew = topNewsData[Math.floor(Math.random()*topNewsData.length)];
-    console.log( 'I m random top new as an object', randomTopNew.title );
-
-    let topNewImg = randomTopNew.multimedia[0].url
-
-    // let d = date.split('T')
-    // console.log('here ', d)
+const TopNews = ({ id, category, title, author, date, snippet, img }) => {
+    let d = date.split('T')
+    console.log('here ', d)
     return (
         // <Link className='link-to-article-details' to={} >
-            <article className='topNew'>
-                <div className='topNew-container'>
-                    <img className='topNew-img' src={ topNewImg } alt={ randomTopNew.title }/>
-                    <div className='p-topNew-container'>
-                        <h3 className='topNew-title'>{ randomTopNew.title }</h3>
-                        <p className='topNew-section'> { randomTopNew.section }</p>
-                        <p className='topNew-author'> { randomTopNew.author }</p>
-                        {/* <p className='randomTopNew-card-date'> { randomTopNew. }</p> */}
-                        <p className='topNew-url'> { randomTopNew.url}</p>
+            <article className='article-details'>
+                <div className='article-details-container'>
+                    <img className='article-details-img' src={ img } alt={ title }/>
+                    <div className='p-details-container'>
+                        <p className='article-details-category'> Section: { category }</p>
+                        <p className='article-details-author'> { author }</p>
+                        <p className='article-details-date'> { date }</p>
+                        <h3 className='article-details-title'>{ title }</h3>
                     </div>
                 </div>
-
             </article>
         // </Link>
     )
 }
+
 
 export default TopNews;
