5f470e55ea6bc4094f049a7e8e70fb20dc890b07
Joana Brito <joanafiorentini@hotmail.com>
2021-11-07 00:14:55 -0700

    Change the logic to find the selected news from the router

--- src/components/AllNews/AllNews.js
+++ src/components/AllNews/AllNews.js
@@ -7,13 +7,18 @@ const AllNews = ({ newsData, findSelectedNews, category }) => {
   const articlesData = newsData.map((article) => {
     let imgUrl = article.multimedia[9].url;
     let img = `https://www.nytimes.com/${imgUrl}`;
+    // console.log(newsData)
+
+
+
+    let new_id = article._id.split('/')[3]
 
 
     // if (article.section_name == category) { 
     return (      
       <NewsCard
-        key={article.id}
-        id={article.id}
+        key={new_id}
+        id={new_id}
         category={article.section_name}
         title={article.headline.main}
         author={article.byline.original}
--- src/components/NewsDetails/NewsDetails.js
+++ src/components/NewsDetails/NewsDetails.js
@@ -1,30 +1,33 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
-import './TopNews.css';
+import './NewsDetails.css';
+import { useParams } from 'react-router-dom'
 
-const NewsDetails = ({  }) => {
 
-    const randomTopNew = topNewsData[Math.floor(Math.random()*topNewsData.length)];
-    let topNewImg = randomTopNew.multimedia[0].url
+const NewsDetails = ({news}) => {
+    const { id } = useParams();
 
-    // let d = date.split('T')
-    // console.log('here ', d)
+    const selectedNews = news.find((article) => article._id.split('/')[3] === id)      
+    let imgUrl = selectedNews.multimedia[9].url;
+    let newsDetailsImg = `https://www.nytimes.com/${imgUrl}`;
+   
     return (
-        // <Link className='link-to-article-details' to={} >
-            <article className='topNew'>
-                <div className='topNew-container'>
-                    <img className='topNew-img' src={ topNewImg } alt={ randomTopNew.title }/>
-                    <div className='p-topNew-container'>
-                        <h3 className='topNew-title'>{ randomTopNew.title }</h3>
-                        <p className='topNew-section'> Section: toUpperCase({ randomTopNew.section })</p>
-                        <p className='topNew-author'> { randomTopNew.author }</p>
-                        {/* <p className='randomTopNew-card-date'> { randomTopNew. }</p> */}
-                        <a className='topNew-url' href ={ randomTopNew.url} >The New York Times Article </a> 
+    
+            <article className='news-details'>
+                <div className='news-details-container'>
+                    <img className='news-details-img' src={ newsDetailsImg } alt={ selectedNews.headline.main }/>
+                    <div className='p-news-details-container'>
+                        <h3 className='news-details-title'>{ selectedNews.headline.main }</h3>
+                        <h4 className='news-details-title'>{ selectedNews.headline.print_headline }</h4>
+                        <p className='news-details-section'> Section: toUpperCase({ selectedNews.section_name })</p>
+                        <p className='news-details-author'> { selectedNews.byline.original }</p>
+                        <p className='news-details-author'> { selectedNews.snippet }</p>
+                        <p className='news-details-author'> { selectedNews.lead_paragraph }</p>
+                        {/* <p className='selectedNews-card-date'> { selectedNews. }</p> */}
+                        <a className='news-details-url' href ={ selectedNews.web_url} >See the entire Artilcle </a> 
                     </div>
                 </div>
-
             </article>
-        // </Link>
     )
 }
 
