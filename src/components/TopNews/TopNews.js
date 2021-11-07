69dd9fa6403805370487e8daf98ab1d4e5472015
Joana Brito <joanafiorentini@hotmail.com>
2021-11-06 21:24:06 -0700

    Add img container

--- src/components/TopNews/TopNews.js
+++ src/components/TopNews/TopNews.js
@@ -5,19 +5,21 @@ const TopNews = ({ topNewsData }) => {
 
     const randomTopNew = topNewsData[Math.floor(Math.random()*topNewsData.length)];
     let topNewImg = randomTopNew.multimedia[0].url
+    let section_name= randomTopNew.section.toUpperCase();
 
     return (
        
             <article className='topNew'>
-                <h3> ------/// Top News ///------ </h3>
                 <div className='topNew-container'>
-                    <img className='topNew-img' src={ topNewImg } alt={ randomTopNew.title }/>
+                    <div className='img-container'>
+                        <img className='topNew-img' src={topNewImg} alt={randomTopNew.title}/>
+                    </div>
                     <div className='p-topNew-container'>
-                        <h3 className='topNew-title'>{ randomTopNew.title }</h3>
-                        <p className='topNew-section'> Section: toUpperCase({ randomTopNew.section })</p>
-                        <p className='topNew-author'> { randomTopNew.author }</p>
+                        <h3 className='topNew-title'>{randomTopNew.title}</h3>
+                        <p className='topNew-section'><strong>{section_name}</strong></p>
+                        <p className='topNew-author'> {randomTopNew.author}</p>
                         {/* <p className='randomTopNew-card-date'> { randomTopNew. }</p> */}
-                        <a className='topNew-url' href ={ randomTopNew.url} >The New York Times Article </a> 
+                        <a className='topNew-url' href ={randomTopNew.url} > See on New York Times</a> 
                     </div>
                 </div>
 
