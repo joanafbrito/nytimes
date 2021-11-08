b90c6c59c1e0d59be1fbd6bbfc4ec9525167be17
Joana Brito <joanafiorentini@hotmail.com>
2021-11-07 21:55:12 -0800

    Delete unnecessary code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Search from '../Search/Search';
 import './Header.css';
 
 const Header = (params) => {
-
     return (
         <header className='header'>
             <Link to='/'>
                 <h1 className='company-name'>NY Times</h1>
             </Link>
             <Search updateCategory= {params.updateCategory}/>
-
         </header>
     )
 }
