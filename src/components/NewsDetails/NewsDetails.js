c0ee13595b7776ba1a899586af890f34ef6144d2
Joana Brito <joanafiorentini@hotmail.com>
2021-11-08 14:35:21 -0800

    Add className to be accordently to the information

--- src/components/NewsDetails/NewsDetails.js
+++ src/components/NewsDetails/NewsDetails.js
@@ -9,6 +9,7 @@ const NewsDetails = ({news}) => {
     const selectedNews = news.find((details) => details._id.split('/')[3] === id)      
     let imgUrl = selectedNews.multimedia[9].url;
     let newsDetailsImg = `https://www.nytimes.com/${imgUrl}`;
+    let sectionName = selectedNews.section_name.toUpperCase();
    
     return (
     
@@ -16,13 +17,12 @@ const NewsDetails = ({news}) => {
                 <div className='news-details-container'>
                     <img className='news-details-img' src={ newsDetailsImg } alt={ selectedNews.headline.main }/>
                     <div className='p-news-details-container'>
-                        <h3 className='news-details-title'>{ selectedNews.headline.main }</h3>
+                        <p className='news-details-section'> <strong>{ sectionName }</strong></p>
+                        <h3 className='news-details-main-title'>{ selectedNews.headline.main }</h3>
                         <h4 className='news-details-title'>{ selectedNews.headline.print_headline }</h4>
-                        <p className='news-details-section'> Section: toUpperCase({ selectedNews.section_name })</p>
                         <p className='news-details-author'> { selectedNews.byline.original }</p>
-                        <p className='news-details-author'> { selectedNews.snippet }</p>
-                        <p className='news-details-author'> { selectedNews.lead_paragraph }</p>
-                        {/* <p className='selectedNews-card-date'> { selectedNews. }</p> */}
+                        <p className='news-details-snippet'> { selectedNews.snippet }</p>
+                        <p className='news-details-lead-paragraph'> { selectedNews.lead_paragraph }</p>
                         <a className='news-details-url' href ={ selectedNews.web_url} >See the entire Artilcle </a> 
                     </div>
                 </div>
