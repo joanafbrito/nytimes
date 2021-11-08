838e7e837b0cf930ffcd060d6138d3cc9c5695c1
Joana Brito <joanafiorentini@hotmail.com>
2021-11-07 21:11:03 -0800

    Delete unnecessary code

--- src/components/AllNews/AllNews.js
+++ src/components/AllNews/AllNews.js
@@ -7,16 +7,9 @@ const AllNews = ({ newsData, findSelectedNews, category }) => {
   const articlesData = newsData.map((article) => {
     let imgUrl = article.multimedia[9].url;
     let img = `https://www.nytimes.com/${imgUrl}`;
-    // console.log(newsData)
-
-
-
     let new_id = article._id.split('/')[3]
     let new_date = article.pub_date.split('T')[0]
-    console.log(new_date)
 
-
-    // if (article.section_name == category) { 
     return (      
       <NewsCard
         key={new_id}
